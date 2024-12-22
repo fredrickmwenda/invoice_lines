c1a711bebc862b73c51c56952ed2d7dfb43b54ec
fredrickmwenda <mwendafredrick31@gmail.com>
2024-12-22 22:40:05 +0300

    added-account-model

---
+++ models/account_move.py
@@ -0,0 +1,10 @@
+# models/account_move.py
+from odoo import models, api
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @api.depends('invoice_line_ids.computed_amount')
+    def _compute_amount_total(self):
+        for invoice in self:
+            invoice.amount_total = sum(line.computed_amount for line in invoice.invoice_line_ids)
